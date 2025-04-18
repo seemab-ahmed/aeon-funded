de63e5c4f79128c74275e378b63e7c89bd3bb383
ashirhash <ashirhashmi26@gmail.com>
2025-04-18 13:11:21 -0400

    35% prmotion banner added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,8 @@ import JoinDiscord from "../components/JoinDiscord";
 import Faqs from "../components/Faqs";
 import { useLocation } from "react-router-dom";
 import OffImg from "../assets/images/off-text.png";
-
-
-const Home = ({mode}) => {
+const Home = ({ mode }) => {
   const location = useLocation();
-
   useEffect(() => {
     if (location.hash) {
       const id = location.hash.replace("#", "");
@@ -30,6 +27,7 @@ const Home = ({mode}) => {
   }, [location]);
   return (
     <main>
+      {" "}
       <div className="px-6 max-md:px-4">
         <Banner mode={mode} />
       </div>
@@ -37,18 +35,26 @@ const Home = ({mode}) => {
       <Success mode={mode} />
       <HowitWorks mode={mode} />
       <PricingPlan mode={mode} />
-      <Secure mode={mode}/>
-      <div className={`w-full ${
-            mode === "dark" ? "" : "filter  grayscale"
-          }`}><img src={OffImg} className="w-full object-cover" alt="Text Off Img" /></div>
+      <Secure mode={mode} />
       <Benifits mode={mode} />
       <div className="px-6 max-md:px-4">
         <Reviews mode={mode} />
       </div>
-      <Faqs mode={mode} />
-      <JoinDiscord mode={mode}/>
+     
+      <Faqs mode={mode} /> 
+      <JoinDiscord mode={mode} />
+      <div
+        className={`w-full  sticky bottom-0 z-[5] ${
+          mode === "dark" ? "" : "filter  grayscale"
+        }`}
+      >
+        <img
+          src={OffImg}
+          className="w-full max-2xl:max-h-24 object-cover"
+          alt="Text Off Img"
+        />
+      </div>
     </main>
   );
 };
-
 export default Home;
